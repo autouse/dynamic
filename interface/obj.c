3e28f5c16781d0b835e45b28ead154431a0382e7
zero <mycon@yandex.ru>
2018-06-30 09:10:21 +0300

    bug

--- interface/obj.c
+++ interface/obj.c
@@ -6,10 +6,21 @@
 //  Copyright © 2018 i. All rights reserved.
 //
 #include <stdio.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
 void obj () {
     puts("c");
 }
-void main () {
+int main (int agrc, char **arcv) {
+    printf("%d \n", agrc);
+    puts(*arcv);
     puts("obj");
+    int id = fork();
+    printf("my id is %d ", id);
+    char *mode = id ? "detected" : NULL;
+    mode[strlen(mode)] = 0;
+    puts(mode);
 }
